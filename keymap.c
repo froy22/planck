a66988b4901c25b5ad8857f704605103c900c871
Froy Terell C. De Leon <froy.deleon@whitecloak.com>
2021-03-04 16:14:27 +0800

    Adjust certain key locations

--- keymap.c
+++ keymap.c
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |      |      | HOME | END  |      |      |   _  |   +  |  [   |   ]  |  |   |
+ * | Del  |      |      | HOME | END  |      |      |      |      |  [   |   ]  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | SHIFT|      |      | PGUP | PGDN |      |      |      |   ,  |   .  |      | SHIFT|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -126,14 +126,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-    KC_DEL,  _______, _______, KC_HOME, KC_END,  _______, _______, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_PIPE,
+    KC_DEL,  _______, _______, KC_HOME, KC_END,  _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_PIPE,
     KC_LSFT, _______, _______, KC_PGUP, KC_PGDN, _______, _______, KC_LPRN, KC_LPRN, KC_DOT,  _______, KC_RSFT,
     _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, TD_SHFT, _______, _______, _______
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
- * |   `  |  F1  |  F2  |  F3  |  F4  |      |      |   -  |   =  |      |      | Bksp |
+ * |   `  |  F1  |  F2  |  F3  |  F4  |      |      |      |      |   -  |   =  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Del  |  F5  |  F6  |  F7  |  F8  |      | LEFT | DOWN |  UP  |RIGHT |      |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -143,7 +143,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, KC_MINS, KC_EQL,  _______,  _______, KC_BSPC,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, KC_MINS,  KC_EQL,  KC_BSPC,
     KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, KC_BSLS,
     KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_LCBR, KC_RCBR, KC_BRID,  KC_BRIU, KC_RSFT,
     _______, KC_LCTL, KC_LALT, KC_LGUI, _______, KC_SPC,  KC_SPC,  _______, TD_SHFT, KC_VOLD,  KC_VOLU, _______
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] = LAYOUT_planck_grid(
     QMK_U,   KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, KC_BTN3, KC_BTN1, KC_BTN2, _______, _______, _______,
     KC_LOCK, _______, KC_ACL0, KC_ACL1, KC_ACL2, _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______,
-    _______, _______, _______, _______, _______, _______, CK_LTAB, CK_RTAB, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, CK_LTAB, CK_RTAB, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
